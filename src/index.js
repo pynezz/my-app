36fc6633665a19b9dda9c1018eb056581b421437
Kevin <kevin.ka_96@hotmail.com>
2021-05-01 18:47:51 +0200

    added component, but failed

--- src/index.js
+++ src/index.js
@@ -7,9 +7,33 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <Clock /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   )
+// }
+// setInterval(tick, 1000);
+
+// function tick() {
+//   ReactDOM.render(
+//     <Clock date={new Date()} />,
+//     document.getElementById('root')
+//   );
+// }
+
+// ReactDOM.render(
+//   <Clock />,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
