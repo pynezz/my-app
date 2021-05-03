3a2301d67b9543ac67b59d765013935e28877fe7
Kevin <kevin.ka_96@hotmail.com>
2021-05-03 12:51:56 +0200

    breaking up components

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,14 +3,14 @@ import React from "react"
 const Form = () => {
     return (
     <form>
-        <input type="text" class="todo-input" />
-        <button class="todo-button" type="submit">
-            <i class="fas fa-plus-square"></i>
+        <input type="text" className="todo-input" />
+        <button className="todo-button" type="submit">
+            <i className="fas fa-plus-square"></i>
         </button>
-        <div class="select">
-            <select name="todos" class="filter-todo">
+        <div className="select">
+            <select name="todos" className="filter-todo">
                 <option value="all">Alle</option>
-                <option value="completed">Completed</option>
+                <option value="completed">Fullført</option>
                 <option value="uncompleted">Ikke fullført</option>
             </select>
         </div>
